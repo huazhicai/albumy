dfecf2a84ae993ded4c0aac17516f1cfaef6b082
huazhicai <936844218@qq.com>
2019-01-04 23:10:12 +0800

    uploads

---
+++ albumy/models.py
@@ -0,0 +1,40 @@
+import os
+from datetime import datetime
+
+from flask import current_app
+from flask_avatars import Identicon
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from albumy.extensions import db, whooshee
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    # 资料
+    username = db.Column(db.String(20), unique=True, index=True)
+    email = db.Column(db.String(254), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(30))
+    website = db.Column(db.String(255))
+    bio = db.Column(db.String(120))
+    location = db.Column(db.String(50))
+    member_since = db.Column(db.DateTime, default=datetime.utcnow)
+    avatar_s = db.Column(db.String(64))
+    avatar_m = db.Column(db.String(64))
+    avatar_l = db.Column(db.String(64))
+    avatar_raw = db.Column(db.String(64))
+    # 用户状态
+    confirmed = db.Column(db.Boolean, default=False)
+    locked = db.Column(db.Boolean, default=False)
+    active = db.Column(db.Boolean, default=True)
+
+    public_collections = db.Column(db.Boolean, default=True)
+    receive_comment_notification = db.Column(db.Boolean, default=True)
+    receive_follow_notification = db.Column(db.Boolean, default=True)
+    receive_collect_notification = db.Column(db.Boolean, default=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    role = db.relationship('Role', back_populates='users')
+    
